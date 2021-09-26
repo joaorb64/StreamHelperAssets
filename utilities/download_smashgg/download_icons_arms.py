35f5c32618a30b70b0594e98d2e0c3b4859b3758
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2021-09-26 15:19:27 +0200

    Improved download from smash.gg script to get character data from the API

--- utilities/download_smashgg/download_icons_arms.py
+++ utilities/download_smashgg/download_icons_arms.py
@@ -1,61 +1,65 @@
 import json
 import requests
+from pathlib import Path
 
-with open("characters.json", 'r') as characters_file:
-    with open("config.json", 'wt') as config_file:
-        with open("README.md", 'wt') as readme_file:
-            with open("downloads/config.json", 'wt') as icon_config_file:
-                game_name = "ARMS"
-                game_id = 193
-                description = "Base config to use this game."
-                credits = ''
+with open("config.json", 'wt') as config_file:
+    with open("README.md", 'wt') as readme_file:
+        Path("downloads").mkdir(parents=True, exist_ok=True)
+        with open("downloads/config.json", 'wt') as icon_config_file:
 
-                characters_file_content = json.load(characters_file)
+            characters_file = requests.get("https://api.smash.gg/characters")
 
-                characters = characters_file_content.get("entities").get("character")
+            game_name = "ARMS"
+            game_id = 193
+            description = "Base config to use this game."
+            credits = ''
 
-                config_dict: dict = {
-                    "name": game_name,
-                    "smashgg_game_id": game_id,
-                    "character_to_codename": {},
-                    "stage_to_codename": {},
-                    "version": "0.1",
-                    "description": f"{description}\n\nCredits: {credits}"
-                }
+            characters_file_content = characters_file.json()
 
-                readme_file_content = f"""
-                # {game_name}
+            characters = characters_file_content.get("entities").get("character")
 
-                ## Description:
-                {description}
+            config_dict: dict = {
+                "name": game_name,
+                "smashgg_game_id": game_id,
+                "character_to_codename": {},
+                "stage_to_codename": {},
+                "version": "0.1",
+                "description": f"{description}\n\nCredits: {credits}"
+            }
 
-                ## Credits:
-                {credits}
-                """.replace("                ", "")
-                icon_config_dict = {
-                    "prefix": "icon_",
-                    "postfix": "_",
-                    "type": ["icon"]
-                }
+            readme_file_content = f"""
+            # {game_name}
 
-                for character in characters:
-                    if character.get("videogameId") == game_id:
-                        name:str = character.get("name")
-                        codename = name.replace(' ', '').replace('&', '').replace('.', '')
-                        value = {
-                            "smashgg_name": name,
-                            "codename": codename
-                        }
-                        config_dict["character_to_codename"][name] = value
-                        for image_data in character.get("images"):
-                            if image_data.get("type") == "stockIcon" and image_data.get("width")==32:
-                                image_file = requests.get(image_data.get("url"))
-                                image_filename = f"icon_{codename}_0.png"
-                                with open(f"downloads/{image_filename}", 'wb') as f:
-                                    f.write(image_file.content)
+            ## Description:
+            {description}
 
-                config_file_content = json.dumps(config_dict, indent=2)
-                config_file.write(config_file_content)
-                readme_file.write(readme_file_content)
-                icon_config_file_content = json.dumps(icon_config_dict, indent=2)
-                icon_config_file.write(icon_config_file_content)
+            ## Credits:
+            {credits}
+            """.replace("            ", "")
+            icon_config_dict = {
+                "prefix": "icon_",
+                "postfix": "_",
+                "type": ["icon"]
+            }
+
+            for character in characters:
+                if character.get("videogameId") == game_id:
+                    name:str = character.get("name")
+                    codename = name.replace(' ', '').replace('&', '').replace('.', '')
+                    value = {
+                        "smashgg_name": name,
+                        "codename": codename
+                    }
+                    config_dict["character_to_codename"][name] = value
+                    for image_data in character.get("images"):
+                        if image_data.get("type") == "stockIcon" and image_data.get("width")==32:
+                            image_file = requests.get(image_data.get("url"))
+                            image_filename = f"icon_{codename}_0.png"
+                            with open(f"downloads/{image_filename}", 'wb') as f:
+                                f.write(image_file.content)
+
+            config_file_content = json.dumps(config_dict, indent=2)
+            config_file.write(config_file_content)
+            readme_file.write(readme_file_content)
+            icon_config_file_content = json.dumps(icon_config_dict, indent=2)
+            icon_config_file.write(icon_config_file_content)

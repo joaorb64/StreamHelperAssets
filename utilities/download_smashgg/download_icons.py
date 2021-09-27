b2c06c61aac63f6297ef16ced5cbd7bc9f975078
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2021-09-27 23:40:07 +0200

    Fused asset download scripts

--- utilities/download_smashgg/download_icons.py
+++ utilities/download_smashgg/download_icons.py
@@ -2,6 +2,21 @@ import json
 import requests
 from pathlib import Path
 
+game_id = 193
+
+with open(f"game_data.json", 'rt') as game_data_file:
+    game_data = json.loads(game_data_file.read())
+found = False
+for game in game_data:
+    if game.get("smashgg_id") == game_id:
+        game_name = game.get("name")
+        image_type = game.get("image_type")
+        found = True
+
+if not found:
+    print("Game not found")
+    exit(1)
+
 download_folder_name = "download"
 Path(download_folder_name).mkdir(parents=True, exist_ok=True)
 
@@ -12,8 +27,6 @@ with open(f"{download_folder_name}/config.json", 'wt') as config_file:
 
             characters_file = requests.get("https://api.smash.gg/characters")
 
-            game_name = "Guilty Gear: Strive"
-            game_id = 33945
             description = "Base config to use this game."
             credits = ''
             version = "1.0"
@@ -58,12 +71,14 @@ with open(f"{download_folder_name}/config.json", 'wt') as config_file:
                         "codename": codename
                     }
                     config_dict["character_to_codename"][name] = value
+                    stored_width = 0
                     for image_data in character.get("images"):
-                        if image_data.get("type") == "stockIcon":
+                        if image_data.get("type") == image_type and stored_width <= image_data.get("width"):
                             image_file = requests.get(image_data.get("url"))
                             image_filename = f"icon_{codename}_0.png"
                             with open(f"{download_folder_name}/icon/{image_filename}", 'wb') as f:
                                 f.write(image_file.content)
+                            stored_width = image_data.get("width")
 
             config_file_content = json.dumps(config_dict, indent=2)
             config_file.write(config_file_content)

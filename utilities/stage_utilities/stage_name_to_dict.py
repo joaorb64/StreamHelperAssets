5da9cc2d70a37e5ae09f134fb3a2b24bbcf94e5f
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2021-10-07 23:34:21 +0200

    Added script to convert stage icon names to part of a base_files config file

---
+++ utilities/stage_utilities/stage_name_to_dict.py
@@ -0,0 +1,15 @@
+import os
+import json
+
+listdir = os.listdir('./')
+
+stage_dict = {}
+
+for file in listdir:
+    if file.endswith('.png'):
+        codename = file.replace(".png", "")
+        stage_dict[codename]={
+            "codename": codename
+        }
+
+print (json.dumps(stage_dict))

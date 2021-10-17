852db807d4f6c50fa36e171e33958f5ccab10568
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2021-10-17 18:22:04 +0200

    Added Game Data for Akatsuki Blitzkampf Ausf. Achse

--- utilities/txt_to_config_json/txt_to_config_json.py
+++ utilities/txt_to_config_json/txt_to_config_json.py
@@ -2,7 +2,7 @@ import json
 import requests
 from pathlib import Path
 
-game_id = 16
+game_id = 5764
 character_list_filename = "characters_list.txt"
 
 with open(f"../download_smashgg/game_data.json", 'rt') as game_data_file:

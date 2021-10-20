16bff24b6e9bc590e0b4d8ed121b267ef6dc96cd
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2021-10-20 10:05:58 +0200

    Added Game Data for Pokken Tournament DX

--- utilities/download_smashgg/download_icons.py
+++ utilities/download_smashgg/download_icons.py
@@ -2,7 +2,7 @@ import json
 import requests
 from pathlib import Path
 
-game_id = 1150
+game_id = 447
 
 with open(f"game_data.json", 'rt') as game_data_file:
     game_data = json.loads(game_data_file.read())

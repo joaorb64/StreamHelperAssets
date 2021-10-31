c63ef18b39647c82c5e772a98cafe0b5c8b10367
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2021-10-31 22:53:42 +0100

    Added game data for Darkstalkers 3

--- utilities/download_smashgg/download_icons.py
+++ utilities/download_smashgg/download_icons.py
@@ -2,7 +2,7 @@ import json
 import requests
 from pathlib import Path
 
-game_id = 33602
+game_id = 582
 
 with open(f"game_data.json", 'rt') as game_data_file:
     game_data = json.loads(game_data_file.read())

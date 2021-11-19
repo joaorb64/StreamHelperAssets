8bf203566c3e0c6ceb5cffd2154b73657656399c
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2021-11-19 21:02:44 +0100

    Added empty file generation to txt_to_config_json.py

--- utilities/txt_to_config_json/txt_to_config_json.py
+++ utilities/txt_to_config_json/txt_to_config_json.py
@@ -2,8 +2,11 @@ import json
 import requests
 from pathlib import Path
 
-game_id = 582
+game_id = 36963
 character_list_filename = "characters_list.txt"
+
+blank_files_folder="blank_files"
+Path(blank_files_folder).mkdir(parents=True, exist_ok=True)
 
 with open(f"../download_smashgg/game_data.json", 'rt') as game_data_file:
     game_data = json.loads(game_data_file.read())
@@ -33,15 +36,17 @@ with open(f"config.json", 'wt') as config_file:
         "credits": str(credits)
     }
 
-    with open(f"{character_list_filename}", 'r') as character_list:
+    with open(f"{character_list_filename}", 'rt', encoding='utf-8') as character_list:
         for character_name in character_list:
             character_name = character_name.replace('\n', '')
             if character_name:
                 codename = character_name.replace(' ', '').replace('&', '').replace(
-                    '.', '').replace('(', '').replace(')', '').replace('-', '')
+                    '.', '').replace('(', '').replace(')', '').replace('-', '').replace("'", "")
                 config_dict["character_to_codename"][character_name] = {
                     "codename": codename
                 }
+                with open(f"{blank_files_folder}/{codename}_0.png", "wb") as blank_character_file:
+                    blank_character_file.write(bytearray())
 
     config_file_content = json.dumps(config_dict, indent=2)
     config_file.write(config_file_content)

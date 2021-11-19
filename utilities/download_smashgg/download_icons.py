dcfd89f13c485d42e0debf60a5cfea2625937aa4
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2021-11-19 21:02:15 +0100

    Added game data for The King of Fighters XV

--- utilities/download_smashgg/download_icons.py
+++ utilities/download_smashgg/download_icons.py
@@ -2,7 +2,7 @@ import json
 import requests
 from pathlib import Path
 
-game_id = 582
+game_id = 36963
 
 with open(f"game_data.json", 'rt') as game_data_file:
     game_data = json.loads(game_data_file.read())

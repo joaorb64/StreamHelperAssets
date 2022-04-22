b025e7e1c5a238c1a63d360b1b31ff4df418308f
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2022-04-22 11:06:32 +0200

    Added assets for Mario Kart 64

--- utilities/generate_eyesights_dict/generate_eyesights_dict.py
+++ utilities/generate_eyesights_dict/generate_eyesights_dict.py
@@ -1,7 +1,7 @@
 import json
 import collections
 
-game_codename = "mbtl"
+game_codename = "mk64"
 
 config_path = f"../../games/{game_codename}/base_files/config.json"
 
--- utilities/txt_to_config_json/txt_to_config_json.py
+++ utilities/txt_to_config_json/txt_to_config_json.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-game_id = 16904
+game_id = 5682
 character_list_filename = "characters_list.txt"
 
 blank_files_folder="blank_files"

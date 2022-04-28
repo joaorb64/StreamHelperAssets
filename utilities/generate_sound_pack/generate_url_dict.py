904473d2c952b0e975c594ba59479b505fb6e641
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2022-04-28 12:25:21 +0200

    Added script to generate a sound pack from a JSON

---
+++ utilities/generate_sound_pack/generate_url_dict.py
@@ -0,0 +1,21 @@
+import json
+import collections
+
+game_codename = "fexl"
+
+config_path = f"../../games/{game_codename}/base_files/config.json"
+
+with open(config_path, "rt", encoding="utf-8") as config_file:
+    config = json.loads(config_file.read())
+
+characters = config.get("character_to_codename")
+
+url_dict = {}
+
+for character_name in characters.keys():
+    url_dict[characters[character_name].get("codename")] = {"url": ""}
+
+url_dict = collections.OrderedDict(sorted(url_dict.items()))
+
+with open("url.json", "wt", encoding="utf-8") as eyesights_file:
+    eyesights_file.write(json.dumps({"sound": url_dict}, indent=2))

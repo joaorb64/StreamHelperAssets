30e1143e747161c5147e8fd3586273aa8bf513eb
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2022-06-15 10:27:10 +0200

    Added assets for Dark Awake

--- utilities/generate_eyesights_dict/generate_eyesights_dict.py
+++ utilities/generate_eyesights_dict/generate_eyesights_dict.py
@@ -1,7 +1,7 @@
 import json
 import collections
 
-game_codename = "sf6"
+game_codename = "dawake"
 
 config_path = f"../../games/{game_codename}/base_files/config.json"
 
--- utilities/txt_to_config_json/txt_to_config_json.py
+++ utilities/txt_to_config_json/txt_to_config_json.py
@@ -1,7 +1,7 @@
 import json
 from pathlib import Path
 
-game_id = 33628
+game_id = 31935
 character_list_filename = "characters_list.txt"
 
 blank_files_folder="blank_files"

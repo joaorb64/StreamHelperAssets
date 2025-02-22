428fbc50ce92be58141227af26df39aad612d304
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2025-02-22 12:25:09 +0100

    UNI2: Added assets for Ogre

--- utilities/fix_scaling_uni2/fix_scaling_uni2.py
+++ utilities/fix_scaling_uni2/fix_scaling_uni2.py
@@ -1,6 +1,7 @@
 from PIL import Image
 from glob import glob
 import json
+import os.path
 
 # file_list = glob("../../games/uni2/full/*.png", recursive=True)
 with open("../../games/uni2/full/config.json", "rt", encoding="utf-8") as config_file:
@@ -12,10 +13,13 @@ rescaling_factor = {}
 for character_name in main_config["character_to_codename"].keys():
     codename = main_config["character_to_codename"][character_name]["codename"]
     image_filename = f"../../games/uni2/full/{full_config['prefix']}{codename}{full_config['postfix']}0.png"
-    image = Image.open(image_filename, "r").convert("RGBA")
-    height = image.height
-    rescaling_factor[codename] = {"0": 2000.0/height}
-    print(codename, rescaling_factor[codename]["0"])
+    if os.path.isfile(image_filename):
+        image = Image.open(image_filename, "r").convert("RGBA")
+        height = image.height
+        rescaling_factor[codename] = {"0": 2000.0/height}
+        print(codename, rescaling_factor[codename]["0"])
+    else:
+        print(f"Could not find {image_filename}")
 full_config["rescaling_factor"] = rescaling_factor
 with open("../../games/uni2/full/config.json", "wt", encoding="utf-8") as config_file:
     config_file.write(json.dumps(full_config, indent=2))

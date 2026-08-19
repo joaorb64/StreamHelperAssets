11d5f39fa6f29dc3b2e4395f481a03291e76cda0
Mathias Lesauvage <lesauvage.mathias@gmx.com>
2026-08-19 16:52:27 +0200

    MTFS: Added scaling

--- utilities/fix_scaling_uni2/fix_scaling_mtfs.py
+++ utilities/fix_scaling_uni2/fix_scaling_mtfs.py
@@ -20,7 +20,7 @@ for character_name in main_config["character_to_codename"].keys():
 
     for i in range(5):
 
-        image_filename = f"../../games/altfg/stand/{stand_config['prefix']}{codename}{stand_config['postfix']}{i:01}.png"
+        image_filename = f"../../games/mtfs/stand/{stand_config['prefix']}{codename}{stand_config['postfix']}{i:01}.png"
         if os.path.isfile(image_filename):
             image = Image.open(image_filename, "r").convert("RGBA")
             height = image.height
@@ -29,7 +29,7 @@ for character_name in main_config["character_to_codename"].keys():
         else:
             print(f"Could not find {image_filename}")
 
-        image_filename = f"../../games/altfg/full/{full_config['prefix']}{codename}{full_config['postfix']}{i:01}.png"
+        image_filename = f"../../games/mtfs/full/{full_config['prefix']}{codename}{full_config['postfix']}{i:01}.png"
         if os.path.isfile(image_filename):
             image = Image.open(image_filename, "r").convert("RGBA")
             height = image.height
@@ -40,7 +40,7 @@ for character_name in main_config["character_to_codename"].keys():
         
 stand_config["rescaling_factor"] = stand_rescaling_factor
 full_config["rescaling_factor"] = full_rescaling_factor
-with open("../../games/altfg/stand/config.json", "wt", encoding="utf-8") as config_file:
+with open("../../games/mtfs/stand/config.json", "wt", encoding="utf-8") as config_file:
     config_file.write(json.dumps(stand_config, indent=2))
-with open("../../games/altfg/full/config.json", "wt", encoding="utf-8") as config_file:
+with open("../../games/mtfs/full/config.json", "wt", encoding="utf-8") as config_file:
     config_file.write(json.dumps(full_config, indent=2))
